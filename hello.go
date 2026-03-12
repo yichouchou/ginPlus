74218d9c7c24de1aa44855a6dd544790c14b58c2
missyl01 <missyl01@zto.com>
2026-03-12 17:48:29 +0800

    fix bind

--- hello.go
+++ hello.go
@@ -28,9 +28,7 @@ type Hello struct {
 // [parm1 string, parm2 string, parm3 int]
 // @GET /block1
 // @resp-custom-user
-// 请求参数: parm1, parm2 (string), parm3 (int), hiValue, hi (struct)
-// 响应内容: "ni hao"
-func (s *Hello) Hi1(parm1 string, parm2 string, parm3 int, hiValue bind.ReqTest, hi *bind.ReqTest) (commentHi1 string, errHi1 error) {
+func (s *Hello) Hi1(parm1 string, parm2 string, parm3 int, hiValue bind.ReqGet, hi *bind.ReqGet) (commentHi1 string, errHi1 error) {
 	fmt.Println("=== Block1 GET ===")
 	fmt.Println("基本参数:", parm1, parm2, parm3)
 	fmt.Println("结构体值:", hiValue)
@@ -40,19 +38,19 @@ func (s *Hello) Hi1(parm1 string, parm2 string, parm3 int, hiValue bind.ReqTest,
 
 // @POST /block2
 // @resp-custom-user
-// 请求参数: hiValue, hi (struct)
-// 响应内容: bind.ReqTest{}
 func (s *Hello) Hi2(hiValue bind.ReqTest, hi *bind.ReqTest) (commentHi2 bind.ReqTest, errHi2 error) {
 	fmt.Println("=== Block2 POST ===")
 	fmt.Println("结构体值:", hiValue)
 	fmt.Println("结构体指针:", hi)
-	return bind.ReqTest{}, nil
-}
+	return bind.ReqTest{
+		Accesstoken: "acc",
+		UserName:    "uname",
+		Password:    "pd",
+		Age:         "ages",
+	}, nil}
 
 // @GET /block3
 // @resp-custom-user
-// 请求参数: name, password (string), age, year (int)
-// 响应内容: 10
 func (s *Hello) Hi3(name, password string, age, year int) (commentHi3 int, errHi3 error) {
 	fmt.Println("=== Block3 GET ===")
 	fmt.Println("多基本类型参数:", name, password, age, year)
@@ -62,19 +60,19 @@ func (s *Hello) Hi3(name, password string, age, year int) (commentHi3 int, errHi
 // {List []bind.ReqTest}
 // @POST /block4
 // @resp-custom-user
-// 请求参数: reqList ([]bind.ReqTest) 结构体数组
-// 响应内容: 4
 func (s *Hello) Hi4(reqList []bind.ReqTest) (index int, errHi4 error) {
-	fmt.Println("=== Block4 POST ===")
-	fmt.Println("结构体数组:", reqList)
+	fmt.Println("这是post请求接受结构体数组")
+	fmt.Println("---post请求，有参数注解，结构体值数组已经调通")
+
+	for i := range reqList {
+		fmt.Println(reqList[i])
+	}
 	return 4, nil
 }
 
 // [reqList *bind.ReqTest]
 // @GET /block5
-// 请求参数: reqList (*bind.ReqTest) 结构体指针
-// 响应内容: 5
-func (s *Hello) Hi5(reqList *bind.ReqTest) (index int, errHi5 error) {
+func (s *Hello) Hi5(reqList *bind.ReqGet) (index int, errHi5 error) {
 	fmt.Println("=== Block5 GET ===")
 	fmt.Println("结构体指针:", reqList)
 	return 5, nil
@@ -82,19 +80,18 @@ func (s *Hello) Hi5(reqList *bind.ReqTest) (index int, errHi5 error) {
 
 // {List []*bind.ReqTest}
 // @POST /block6
-// 请求参数: reqList ([]*bind.ReqTest) 结构体指针数组
-// 响应内容: 6
 func (s *Hello) Hi6(reqList []*bind.ReqTest) (index int, errHi4 error) {
-	fmt.Println("=== Block6 POST ===")
-	fmt.Println("结构体指针数组:", reqList)
+	fmt.Println("这是post请求接受指针结构体数组")
+	fmt.Println("---post请求，有参数注解，结构体指针数组已经调通")
+	for i := range reqList {
+		fmt.Println(reqList[i])
+	}
 	return 6, nil
 }
 
 // [reqList bind.ReqTest]
 // @GET /block7
-// 请求参数: reqList (bind.ReqTest) 结构体值
-// 响应内容: 5
-func (s *Hello) Hi7(reqList bind.ReqTest) (index int, errHi5 error) {
+func (s *Hello) Hi7(reqList bind.ReqGet) (index int, errHi5 error) {
 	fmt.Println("=== Block7 GET ===")
 	fmt.Println("结构体值:", reqList)
 	return 5, nil
